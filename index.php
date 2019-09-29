30df1b28d614c867ef85d73ab099bc899bfdb656
Lumenebris <55246829+Lumenebris@users.noreply.github.com>
2019-09-29 18:48:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -23,60 +23,66 @@ if (is_null($_SESSION['i'])) {
 			<h3>Вариант: 202016</h3>
 		</header>
 		<div class="content">
-			<div class="leftSidebar">
+			<div class="upperSidebar">
 				<form action="script.php" method="get" target="result">
-					<p><b>Выберите значение R:</b>
-						<p><input type="radio" name="r" value="1" checked> 1
-						<input type="radio" name="r" value="2"> 2
-						<input type="radio" name="r" value="3"> 3
-						<input type="radio" name="r" value="4"> 4
-						<input type="radio" name="r" value="5"> 5</p>
-					</p>
-					<p><b>Введите значение X:</b>
-						<p><input id="text_x" type="text" size="15" name="x" placeholder="от -3 до 5"></p>
-					</p>
-					<p><b>Выберите значение Y:</b>
-						<div class="choose_y">
-							<table>
-								<tr>
-									<td><input type="checkbox" id="y1" name="y" value="-4"> -4</td>
-									<td><input type="checkbox" id="y2" name="y" value="-3"> -3</td>
-									<td><input type="checkbox" id="y3" name="y" value="-2"> -2</td>
-								</tr>
-								<tr>
-									<td><input type="checkbox" id="y4" name="y" value="-1"> -1</td>
-									<td><input type="checkbox" id="y5" name="y" value="0"> 0</td>
-									<td><input type="checkbox" id="y6" name="y" value="1"> 1</td>
-								</tr>
-								<tr>
-									<td><input type="checkbox" id="y7" name="y" value="2"> 2</td>
-									<td><input type="checkbox" id="y8" name="y" value="3"> 3</td>
-									<td><input type="checkbox" id="y9" name="y" value="4"> 4</td>
-								</tr>
-							</table>
-						</div>
-					</p>
+					<div class="chooseR">
+						<p class="heading"><b>Выберите значение R:</b>
+							<p><input type="radio" name="r" value="1" checked> 1
+							<input type="radio" name="r" value="2"> 2
+							<input type="radio" name="r" value="3"> 3
+							<input type="radio" name="r" value="4"> 4
+							<input type="radio" name="r" value="5"> 5</p>
+						</p>
+					</div>
+					<div class="inputX">
+						<p class="heading"><b>Введите значение X:</b>
+							<p><input id="text_x" type="text" size="15" name="x" placeholder="от -3 до 5" onblur="verifyX(this)" oninput="verifyX(this)"></p>
+							<p id="message" style="visibility: hidden">.</p>
+						</p>
+					</div>
+					<div class="chooseY">
+						<p class="heading"><b>Выберите значение Y:</b>
+								<table>
+									<tr>
+										<td><input type="checkbox" id="y1" name="y[]" value="-4"> -4</td>
+										<td><input type="checkbox" id="y2" name="y[]" value="-3"> -3</td>
+										<td><input type="checkbox" id="y3" name="y[]" value="-2"> -2</td>
+									</tr>
+									<tr>
+										<td><input type="checkbox" id="y4" name="y[]" value="-1"> -1</td>
+										<td><input type="checkbox" id="y5" name="y[]" value="0"> 0</td>
+										<td><input type="checkbox" id="y6" name="y[]" value="1"> 1</td>
+									</tr>
+									<tr>
+										<td><input type="checkbox" id="y7" name="y[]" value="2"> 2</td>
+										<td><input type="checkbox" id="y8" name="y[]" value="3"> 3</td>
+										<td><input type="checkbox" id="y9" name="y[]" value="4"> 4</td>
+									</tr>
+								</table>
+						</p>
+					</div>
 					<div class="buttons">
 						<table>
 							<tr>
 								<td>
-									<input type="button" class="check_button" value=" Проверить " OnClick="check()">
+									<input type="button" id="button" value=" Проверить " OnClick="check()">
 								</td>
 								<td>
-									<p><input type="submit" class="submit_button" id="submit" value=" Результат " disabled></p>
+									<input type="submit" id="submit"  OnClick="onSubmit()" value=" Результат " disabled>
 								</td>
 							</tr>
 						</table>
 					</div>
 					<p id="error"></p>
 				</form>
+				<img src="images/image.png">
 			</div>
-			<div class="rightSidebar">
+			<div class="bottomSidebar">
 				<div id="frame" class="container">
-					<iframe name = "result" height="550"></iframe>
+					<iframe name = "result"></iframe>
 				</div>
 			</div>
 		</div>
-		<footer>Copyright © Lumenebris, 2019</footer>
+		<div class="footer">Copyright © <a href="https://github.com/Lumenebris/web_lab1">Lumenebris</a>, 2019</div>
     </body>
 </html>
