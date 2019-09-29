7317fa8d25a635429b4014fc65e8dd5eef764c6a
Lumenebris <55246829+Lumenebris@users.noreply.github.com>
2019-09-29 18:50:02 +0300

    Update check.js

--- js/check.js
+++ js/check.js
@@ -1,63 +1,100 @@
-function check() {
-	var error = document.getElementById("error");
-	error.innerHTML = "";
+//сообщение для x
+function message(msg) {
+   var node = document.getElementById('message');
+    node.innerText = msg;
+    node.style.visibility = 'visible';
+}
+
+//проверка поля x
+function verifyX(x) {
 	const xMax = 5;
 	const xMin = -3;
-	var x = "";
-	var xInput = document.getElementById("text_x");
-	var yArray = Array.from(document.querySelectorAll('[id="y1"], [id="y2"], [id="y3"], [id="y4"], [id="y5"], [id="y6"], [id="y7"], [id="y8"], [id="y9"]'));
-	var flag_y = 1;
-	var flag_x = 2;
-	document.getElementById("submit").disabled = 1;
+    let pattern = new RegExp(/^(-?0$|-?[1-9]*([,.]\d*$)?|-?0[,.]\d*$)$/);
+    let xValue = parseFloat(x.value.replace(/,/, '.'));
+    let elem = document.getElementById('text_x');
+	let flag = 0;
+    if (x.value != '' && x.value != '-') {
+		if (isNaN(xValue)) {
+			x.focus();
+            elem.style.backgroundColor = '#bb99ff';
+			message('х должен быть числом');
+			x.value = '';
+            return false;
+		} else if (!pattern.test(x.value)) {
+            x.focus();
+            elem.style.backgroundColor = '#bb99ff';
+			message('х должен быть числом');
+			x.value = '';
+            return false;
+        } else if (xValue < xMin || xValue > xMax) {
+			x.focus();
+            elem.style.backgroundColor = '#bb99ff';
+			message('x вне диапазона');
+			x.value = '';
+            return false;
+		} else if (x.value.length > 5) {
+			x.focus();
+            elem.style.backgroundColor = '#bb99ff';
+			message('длина x не должна превышать 5');
+			x.value = '';
+            return false;
+		}
+        elem.style.backgroundColor = '#FFF';
+		document.getElementById('message').style.visibility = 'hidden';
+        return true;
+    }
+    elem.style.backgroundColor = '#FFF';
+	document.getElementById('message').style.visibility = 'hidden';
+    return true;
+}
+
+//проверка x и y
+function check() {
+	let error = document.getElementById('error');
+	error.innerHTML = '';
+	let yArray = Array.from(document.querySelectorAll('[id="y1"], [id="y2"], [id="y3"], [id="y4"], [id="y5"], [id="y6"], [id="y7"], [id="y8"], [id="y9"]'));
+	let pat = new RegExp(/^(-?0[,.]0*$)$/);
+    let xInput = document.getElementById('text_x');
+	let flag_y = 0;
+	let flag_x = 0;
+	
+	//проверка x
+	if (xInput.value != '') {
+		if (pat.test(xInput.value)) {
+			xInput.value = '0';
+		}
+		flag_x = 1;
+	} 
+	
+	//если x не введен
+	if (!flag_x) {
+		let err_x = document.createElement('div');
+		err_x.innerHTML = 'Не введено значение x';
+		error.append(err_x);
+	}
+	
 	//проверка y
 	for (y of yArray) {
 		if (y.checked) {
-			flag_y = 0;
+			flag_y = 1;
 			//yCount++;
 		}
 	}
+	
 	//если y не выбран
-	if (flag_y) {
+	if (!flag_y) {
 		let err_y = document.createElement('div');
 		err_y.innerHTML = 'Не выбрано значение y';
 		error.append(err_y);
 	}
-	//проверка x
-	var pattern = new RegExp(/^(0$|-?[1-9]\d*([,.]\d*[1-9]$)?|-?0[,.]\d*[1-9])$/);
-	if (xInput.value) {
-		if (xInput.value.length < 6) {
-			for (i = 0; i < xInput.value.length; i++) {
-				if (xInput.value[i] != ' ') {
-					x += String(xInput.value[i].replace(',', '.'));
-				} else {
-					flag_x = 4;
-					i = xInput.value.length;
-				}
-			}
-			if (pattern.test(x)) flag_x = 0;
-			else flag_x = 2;
-			if (flag_x != 2 & flag_x != 4 ) {
-				if ((x < xMin) || (x > xMax)) flag_x = 3;
-			}
-		} else flag_x = 5;
-	} else flag_x = 1;
-	//если x неправильный
-	if (flag_x) {
-		let err_x = document.createElement('div');
-		if (flag_x == 1)
-			err_x.innerHTML = "Не выбрано значение x";
-		if (flag_x == 2)
-			err_x.innerHTML = "Ошибка в значении x";
-		if (flag_x == 3)
-			err_x.innerHTML = "x вне диапазона";
-		if (flag_x == 4)
-			err_x.innerHTML = "х должен быть числом";
-		if (flag_x == 5)
-			err_x.innerHTML = "Длина x не должне превышать 5";
-			error.append(err_x);
-	}
+	
 	//если x и y правильные
-	if (flag_x == 0 && flag_y == 0) {
+	if (flag_x && flag_y) {
 		document.getElementById("submit").disabled = 0;
 	}
+}
+
+//блокировка кнопки Результат
+function onSubmit() {
+	setTimeout(function () { document.getElementById("submit").disabled = 1; }, 2000);
 }
